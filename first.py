5278ce94015a2d0c26cdce225bb8cbc5cd687eec
James Ngugi <53422918+Jamesdev-ke@users.noreply.github.com>
2022-07-28 21:47:08 +0300

    Create first.py
    Add sections to python file.

---
+++ first.py
@@ -0,0 +1,77 @@
+#!/bin/python3
+
+#Print string
+print("Hello, world!")
+print('Hello, world!')
+print("""This string runs
+multiple lines!""") #triple quote for multi-line print
+("This string is "+" awesome!") #We can also concatenate
+print('\n') #new line
+print('Test that new line out.')
+
+#MATH
+print (5050) #add
+print(50-50) #subtract
+print (50* 50) #multiply print(50/50) #divide
+print(5050 50 50/50) #PEMDAS -
+print(50 ** 2) #xponents
+print(50 % 6) #modulo - takes what is left over
+print(506) #division with remainder (or a float)
+print(50 // 6) #no remainder
+
+print(/n)
+#VARIABLES AND METHODS
+quote = "All is fair in love and war." print(quote)
+print(quote.upper()) #uppercase print(quote. lower()) #lowercase
+print(quote.title()) #title case
+print(len(quote)) #counts characters
+name="Heath" #string gpa = 3.7 #float has a decimal -
+age 33 #int
+print(int(age))
+print(int(30.1))
+print(int(30.9)) #Will it round? NO.
+
+print("My name is + name + and I am + str(age) + years old.")
+
+age += 1
+print(age)
+
+birthday = 1
+age+=birthday
+print(age)
+
+print(/n)
+
+#FUNCTIONS
+def who_am_1():#this is a function without parameters
+  name="Heath" #local variable
+  age = 30 print("My name is + name + and I am + str(age) + " years old.")
+
+who_am_i()
+
+def add_one_hundred (num):
+  print(num + 100)
+                 
+add_one_hundred (100)
+                 
+def add(x,y):
+  print(x + y)
+
+add (7,7)
+
+def multiply(x,y):
+  return x * y
+print(multiply(7,7))
+
+def square_root(x):
+  print(x ** .5)
+                 
+square_root (64)
+
+def nl(): #new line print
+  ('\n')
+   
+nl()
+
+#BOOLEAN EXPRESSIONS (TRUE OR FALSE)
+
